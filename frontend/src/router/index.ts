296d272589f9e7e922c63263268d3fea8180fbf3
adrinomik <adrinomik@outlook.com>
2025-07-08 05:31:44 +0300

    Learning VUE, starting in playground.vue

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
-
+import Playground from '../views/Playground.vue'
 const routes = [
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/playground', component: Playground}
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Playground.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="max-width: 1200px; margin: 0 auto;">
+    <h1>Playground</h1>
+    <p>Счетчик: {{ count }}</p>
+
+    <button @click="increment" :disabled="count >= 10">Увеличить</button>
+    <button @click="decrement" :disabled="count <= -5">Уменьшить</button>
+    <button @click="reset">Сбросить</button>
+
+    
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function reset() {
+    count.value = -2
+}
+
+function increment() {
+    if (count.value < 10) {
+        count.value++
+    }
+}
+
+function decrement() {
+    if (count.value > -5) {
+        count.value--
+    }
+}
+
+</script>
